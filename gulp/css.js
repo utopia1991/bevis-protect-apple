2a641f8048e12c1627deaa41670a52602ab3b4f7
Bevis Shen <1304354608@qq.com>
2017-03-22 22:22:46 +0800

    add all

--- gulp/css.js
+++ gulp/css.js
@@ -1,35 +1,13 @@
 // 载入需要的依赖和插件
 var gulp = require('gulp'),
 	less = require('gulp-less'),                    // less 编译成 css
-	rev = require('gulp-rev'),                      // 对文件名加MD5后缀
-	revCollector = require('gulp-rev-collector'),   // 路径替换
-	del = require('del'),                           // 删除文件
 
-	_srcname = './client',
-	_dirname = './assets',
-	_rev = './rev',
-	cssSrc = _srcname + '/less/**/*',
-	cssRevSrc = _srcname + '/less/revCss',
-	cssDest = _dirname + '/client/less';
+	cssSrc = './client/less/**/*',
+	cssDest = './assets/less/';
 
 // 合并 CSS,生成版本号
 gulp.task('less', function () {
-	return gulp.src(cssDest + '/**/*')
+	return gulp.src(cssSrc)
 		.pipe(less())                       // less 转 css
-		.pipe(gulp.dest(_dirname + '/less'))           // 直接输出到目标路径
+		.pipe(gulp.dest(cssDest))           // 直接输出到目标路径
 });
-
-gulp.task('revCss', function () {
-	return gulp.src([_rev + '/**/*.json', cssSrc])
-		.pipe(revCollector())
-		.pipe(gulp.dest(cssRevSrc));
-});
-
-gulp.task('delRevCss', function(){
-	del([
-		cssRevSrc,
-		_rev
-	]);
-});
-
-gulp.task('css',['miniCss','revCss']);
